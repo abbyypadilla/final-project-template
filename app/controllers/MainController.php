2d58271cfbb55c860eef5507bf704f848e9f4d1a
abbyypadilla <apadilla26@fordham.edu>
2024-12-03 22:52:39 -0500

    Update MainController.php

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,14 +3,20 @@
 namespace app\controllers;
 
 class MainController extends Controller {
-
+    
     public function homepage() {
-        //remember to route relative to index.php
-        //require page and exit to return an HTML page
         $this->returnView('./assets/views/main/homepage.html');
     }
 
-    public function notFound() {
+    public function about() {
+        $this->returnView('./assets/views/main/about.html');
+    }
+    
+    public function contact() {
+        $this->returnView('./assets/views/main/contact.html');
     }
 
+    public function portfolio() {
+        $this->returnView('./assets/views/projects/portfolio.html');
+    }
 }
