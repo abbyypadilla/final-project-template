7a060ed47829ed8fc488ca751759a8cfc0805254
abbyypadilla <apadilla26@fordham.edu>
2024-12-03 23:39:16 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,16 @@
 <?php
 
-require '../app/core/setup.php';
+require '../app/core/setup.php'; 
 use app\core\Router;
+use app\models\PortfolioModel; 
+
 $router = new Router();
 
-?>
+$portfolioModel = new PortfolioModel();
 
+$projects = $portfolioModel->getAllProjects();
+if (!is_array($projects)) {
+    $projects = [];
+}
+
+?>
