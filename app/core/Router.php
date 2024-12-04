c22e29a198c92311e63b96688c8b7121d6b25d4c
abbyypadilla <apadilla26@fordham.edu>
2024-12-03 23:09:44 -0500

    Update Router.php

--- app/core/Router.php
+++ app/core/Router.php
@@ -3,16 +3,20 @@
 namespace app\core;
 
 use app\controllers\MainController;
-use app\controllers\UserController;
+use app\controllers\PortfolioController;
+use app\controllers\ContactController;  
+
+require_once '../app/models/ContactModel.php';
 
 class Router {
     public $urlArray;
 
-    function __construct()
-    {
+    function __construct() {
         $this->urlArray = $this->routeSplit();
         $this->handleMainRoutes();
-        $this->handleUserRoutes();
+        $this->handlePortfolioRoutes();  
+        $this->handlePageRoutes();
+        $this->handleContactRoutes();  
     }
 
     protected function routeSplit() {
@@ -27,16 +31,39 @@ class Router {
         }
     }
 
-    protected function handleUserRoutes() {
-        if ($this->urlArray[1] === 'users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-            $userController = new UserController();
-            $userController->usersView();
-        }
-
-        //give json/API requests a api prefix
-        if ($this->urlArray[1] === 'api' && $this->urlArray[2] === 'users' && $_SERVER['REQUEST_METHOD'] === 'GET') {
-            $userController = new UserController();
-            $userController->getUsers();
+    protected function handlePortfolioRoutes() {
+        $portfolioController = new PortfolioController();
+        if ($this->urlArray[1] === 'portfolio' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+            $portfolioController->getProjects();
         }
     }
+
+    protected function handlePageRoutes() {
+        $mainController = new MainController();
+
+        if ($this->urlArray[1] === 'about' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+            $mainController->about();
+        }
+
+        if ($this->urlArray[1] === 'contact' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+            $mainController->contact();
+        }
+
+        if ($this->urlArray[1] === 'portfolio' && $_SERVER['REQUEST_METHOD'] === 'GET') {
+            $mainController->portfolio();
+        }
+    }
+
+    protected function handleContactRoutes() {
+        //var_dump($this->urlArray); used for testing purposes
+        echo '<br>';
+        if ($this->urlArray[1] === 'contact' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+            $contactController = new ContactController();
+            $contactController->submitContactForm();
+        }
+    }
+    
+    
 }
+
+?>
