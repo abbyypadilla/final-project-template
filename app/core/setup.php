0576f8623943637c727d1b4fb43a0cc216d60250
abbyypadilla <apadilla26@fordham.edu>
2024-12-03 23:10:36 -0500

    Update setup.php

--- app/core/setup.php
+++ app/core/setup.php
@@ -1,15 +1,16 @@
 <?php
 
-//require our files, remember should be relative to index.php
 require '../app/core/Router.php';
 require '../app/models/Model.php';
 require '../app/controllers/Controller.php';
-require '../app/controllers/MainController.php';
-require '../app/controllers/UserController.php';
+
+require '../app/models/PortfolioModel.php';
 require '../app/models/User.php';
 
+require '../app/controllers/MainController.php';
+require '../app/controllers/PortfolioController.php';
+require '../app/controllers/ContactController.php';
 
-//set up env variables
 $env = parse_ini_file('../.env');
 
 define('DBNAME', $env['DBNAME']);
@@ -18,5 +19,4 @@ define('DBUSER', $env['DBUSER']);
 define('DBPASS', $env['DBPASS']);
 define('DBDRIVER', '');
 
-//set up other configs
 define('DEBUG', true);
